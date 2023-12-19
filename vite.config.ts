1b746138cc537dddf04770d7e6cc611cb98811e1
菊池雅英 <m20000406k@gmail.com>
2023-12-19 16:05:42 +0900

    feat: github action

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+    plugins: [vue()],
+    root: ".",
+    publicDir: "../public",
+    build: {
+        outDir: "./dist",
+        assetsDir: "./assets",
+        copyPublicDir: true,
+        rollupOptions: {
+            input: {
+                main: "./index.html",
+            },
+        },
+    },
+});
