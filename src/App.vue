b659d85dcbfc5ed043df839fb2127159c1464f13
菊池雅英 <m20000406k@gmail.com>
2023-12-19 16:12:36 +0900

    fix:

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from "vue";
+import { sum } from "./sum";
 
-<template></template>
+const comp = computed(() => sum(1, 2));
+</script>
+
+<template>1+2={{ comp }}</template>
 
 <style scoped></style>
